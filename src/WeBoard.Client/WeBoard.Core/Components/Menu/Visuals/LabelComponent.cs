0c73e015666274d19b1a5d700cc59e5899d97a62
Anton Markelov <114746674+antonmarkel@users.noreply.github.com>
2025-05-25 19:19:33 +0300

    Update label

--- src/WeBoard.Client/WeBoard.Core/Components/Menu/Visuals/LabelComponent.cs
+++ src/WeBoard.Client/WeBoard.Core/Components/Menu/Visuals/LabelComponent.cs
@@ -4,13 +4,19 @@ using WeBoard.Core.Components.Base;
 
 namespace WeBoard.Core.Components.Menu.Visuals
 {
-    public class MenuTextComponent : ComponentBase
+    public class LabelComponent : MenuComponentBase
     {
         private readonly Text _text;
         private readonly Font _font;
 
         private readonly RectangleShape _shape = new();
         protected override Shape Shape => _shape;
+
+        public Color BackgroundColor
+        {
+            get => _shape.FillColor;
+            set => _shape.FillColor = value;
+        }
 
         public string Content
         {
@@ -21,12 +27,16 @@ namespace WeBoard.Core.Components.Menu.Visuals
         public override Vector2f Position
         {
             get => _text.Position;
-            set => _text.Position = value;
+            set
+            {
+                _text.Position = value;
+                base.Position = value;
+            }
         }
 
         public override FloatRect GetGlobalBounds() => _text.GetGlobalBounds();
 
-        public MenuTextComponent(string content, Vector2f position, uint fontSize = 24)
+        public LabelComponent(string content, Vector2f position, uint fontSize = 24)
         {
             _font = new Font("C:/Windows/Fonts/arial.ttf");
             _text = new Text(content, _font, fontSize)
@@ -34,11 +44,26 @@ namespace WeBoard.Core.Components.Menu.Visuals
                 FillColor = Color.Black,
                 Position = position
             };
+
+            _shape.Size = _text.GetGlobalBounds().Size * 1.05f;
+            BackgroundColor = Color.Transparent;
+            _shape.Position = position;
         }
 
         public override void Draw(RenderTarget target, RenderStates states)
         {
+            target.Draw(_shape);
             target.Draw(_text, states);
+        }
+
+        public override FloatRect GetScreenBounds()
+        {
+            return _text.GetGlobalBounds();
+        }
+
+        public override void OnClick(Vector2f offset)
+        {
+           
         }
     }
 }
